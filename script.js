6b35967ec1036131d566f8aafcb1fbef662e3206
antelor <ramiro.antelo4@gmail.com>
2021-05-30 14:22:59 -0300

    juego funcionando

--- script.js
+++ script.js
@@ -6,19 +6,37 @@ computerPlay = () => {
 round = (playerSelection, computerSelection) => {
     pS=playerSelection.toLowerCase()
 
-    if(pS == computerSelection) return "Empate!"
+    if(pS == computerSelection) return "Empata la ronda!"
     if(
         (pS == "piedra" && computerSelection == "papel") ||
         (pS == "papel" && computerSelection == "tijera") ||
         (pS == "tijera" && computerSelection == "piedra")
-        ) return `Perdiste, ${computerSelection} le gana a ${pS}.`
+        ) return `Perdiste la ronda, ${computerSelection} le gana a ${pS}.`
     if(
         (pS == "piedra" && computerSelection == "tijera") ||
         (pS == "papel" && computerSelection == "piedra") ||
         (pS == "tijera" && computerSelection == "papel")
-        ) return `Ganaste, ${pS} le gana a ${computerSelection}.`
+        ) return `Ganaste la ronda, ${pS} le gana a ${computerSelection}.`
+    else return `Opcion no valida! Gana la computadora`
 }
 
-const playerSelection = "piedra";
-const computerSelection = computerPlay();
-console.log(round(playerSelection, computerSelection));
+game = () => {
+    let puntosCompu=0
+    let puntosPlayer=0
+    for(let i=0; i<5;i++){
+        let playerSelection = window.prompt("Piedra, papel o tijera?");
+        let computerSelection = computerPlay();
+
+        let roundResult = round(playerSelection, computerSelection)
+        console.log(roundResult);
+        if(roundResult[0]==`P` || roundResult[0]==`O`) puntosCompu++
+        else puntosPlayer++
+    }
+
+    if(puntosCompu==puntosPlayer)    console.log("Empate!!");
+    else if(puntosCompu<puntosPlayer) console.log("Ganaste!!")
+    else console.log("Gana la computadora!!")
+
+}
+
+game()
