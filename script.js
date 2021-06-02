ac4a707384591ac7b3d51347ee177c1bf1a53a70
antelor <ramiro.antelo4@gmail.com>
2021-06-02 13:09:21 -0300

    muestra de resultados funcionando

--- script.js
+++ script.js
@@ -1,37 +1,67 @@
 computerPlay = () => {
-    let choices=["piedra", "papel", "tijera"]
+    let choices = ["piedra", "papel", "tijera"]
     return choices[Math.floor(Math.random() * 3)];
 }
 
-round = (playerSelection, computerSelection) => {
+round = (e) => {
+    let computerSelection = computerPlay()
+    let playerSelection = e.target.name;
+
     pS=playerSelection.toLowerCase()
 
-    if(pS == computerSelection) return "Empata la ronda!"
-    if(
+    if(pS == computerSelection){
+        //return "Empata la ronda!"
+        const resultadoRonda = document.createElement('div');
+        resultadoRonda.classList.add('resultadoRonda');
+        resultadoRonda.textContent = "Empata la ronda!";
+
+        resultado.appendChild(resultadoRonda);
+    } 
+    else if(
         (pS == "piedra" && computerSelection == "papel") ||
         (pS == "papel" && computerSelection == "tijera") ||
         (pS == "tijera" && computerSelection == "piedra")
-        ) return `Perdiste la ronda, ${computerSelection} le gana a ${pS}.`
-    if(
+        ) {
+            //return `Perdiste la ronda, ${computerSelection} le gana a ${pS}.`
+            const resultadoRonda = document.createElement('div');
+            resultadoRonda.classList.add('resultadoRonda');
+            resultadoRonda.textContent = `Perdiste la ronda, ${computerSelection} le gana a ${pS}.`;
+
+            resultado.appendChild(resultadoRonda);
+        }
+    else if(
         (pS == "piedra" && computerSelection == "tijera") ||
         (pS == "papel" && computerSelection == "piedra") ||
         (pS == "tijera" && computerSelection == "papel")
-        ) return `Ganaste la ronda, ${pS} le gana a ${computerSelection}.`
-    else return `Opcion no valida! Gana la computadora`
+        ) {
+            //return `Ganaste la ronda, ${pS} le gana a ${computerSelection}.`
+            const resultadoRonda = document.createElement('div');
+            resultadoRonda.classList.add('resultadoRonda');
+            resultadoRonda.textContent = `Ganaste la ronda, ${pS} le gana a ${computerSelection}.`;
+
+            resultado.appendChild(resultadoRonda);
+        }
+    else {
+        //return `Opcion no valida! Gana la computadora`
+        const resultadoRonda = document.createElement('div');
+        resultadoRonda.classList.add('resultadoRonda');
+        resultadoRonda.textContent = `Opcion no valida! Gana la computadora`;
+
+        resultado.appendChild(resultadoRonda);       
+    }
 }
 
-game = () => {
+game = (playerSelection) => {
     let puntosCompu=0
     let puntosPlayer=0
-    for(let i=0; i<5;i++){
-        let playerSelection = window.prompt("Piedra, papel o tijera?");
+    /**for(let i=0; i<5;i++){*/
         let computerSelection = computerPlay();
 
         let roundResult = round(playerSelection, computerSelection)
         console.log(roundResult);
         if(roundResult[0]==`P` || roundResult[0]==`O`) puntosCompu++
         else puntosPlayer++
-    }
+    //}
 
     if(puntosCompu==puntosPlayer)    console.log("Empate!!");
     else if(puntosCompu<puntosPlayer) console.log("Ganaste!!")
@@ -39,4 +69,11 @@ game = () => {
 
 }
 
-game()
+const buttpiedra = document.querySelector('button[class=buttpiedra]');
+const buttpapel = document.querySelector('button[class=buttpapel]');
+const butttijera = document.querySelector('button[class=butttijera]');
+const resultado = document.querySelector('[class=resultado]');
+
+buttpiedra.addEventListener('click', round);
+buttpapel.addEventListener('click', round);
+butttijera.addEventListener('click', round);
